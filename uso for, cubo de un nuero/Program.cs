01315c54be16e27692bfeb9ebff06dae10fa653a
Raul Gomez <ragomez1234567@gmail.com>
2021-10-21 17:28:01 -0600

    Add project files.

---
+++ uso for, cubo de un nuero/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace uso_for__cubo_de_un_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("introduce el " + i + " numero: ");
+                num = double.Parse(Console.ReadLine());
+                num = Math.Pow(num, 3);
+                Console.WriteLine("el cubo es: " + num);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ uso for, cubo de un nuero/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("uso for, cubo de un nuero")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("uso for, cubo de un nuero")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("5293d03d-ed4b-46f4-b5cc-42b3c8a3c2e5")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
